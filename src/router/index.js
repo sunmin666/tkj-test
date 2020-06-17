844bf25f21034ed3c243f9a7a94da51593676165
yhb <oldchineseboys@163.com>
2020-06-17 13:56:25 +0800

    src\router# On branch yanghuabin

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Yang from '../views/Yang.vue'
 Vue.use(VueRouter)
 
   const routes = [
@@ -16,7 +16,11 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
+  },
+  {
+    path:'/yang',
+    component: Yang
   }
 ]
 
---
+++ src/views/Yang.vue
@@ -0,0 +1,7 @@
+<template>
+  <div> 
+        my name is 杨华彬. 
+        I form xingtaishihebeisheng.
+        I have a girlfriend. I'm very </div>
+</template>
+

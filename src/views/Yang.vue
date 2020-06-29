26c742e33f599f315e53b18a7a44a378cdee0c8b
yhb <oldchineseboys@163.com>
2020-06-29 15:28:34 +0800

    helooword home yang app

--- src/views/Yang.vue
+++ src/views/Yang.vue
@@ -1,7 +1,72 @@
 <template>
-  <div> 
-        my name is 杨华彬. 
-        I form xingtaishihebeisheng.
-        I have a girlfriend. I'm very </div>
+  <div class="warp"> 
+    <div class="header">
+        3000元详情
+    </div>
+    <div class="content">
+      <span>1.跑步机功能操作的舒适性：这个主要根据各自使用跑步机的体验性是否良好。</span>
+      <span>2.跑步机的跑步面积：这主要影响到我们跑步时候的舒适性，大空间的跑步机跑起来更舒服，主要可以参考跑步板的宽度和跑步板的稳固性。</span>
+      <span>3.预设程序是否满足需要：我们在跑步锻炼的时候一些数据是需要随时监控的，这些预设功能是否满足你自身的需求，在跑步机体验的时候最好把所有预设程序都体验一下。</span>
+      <span>4.跑步锻炼时的安全性如何：我们使用跑步机进行锻炼的目的就是为了身体的健康，因此跑步机的安全性是我们必须要考虑的。一般可以从跑步机扶手的设计是否方便的且舒适以及急停按钮是否容易触及和灵活。</span>
+      <span>5.跑步机面板设计是否人性化：当我们在跑步机上锻炼的时候，跑步上的信息是我们随时关注的。因此跑步机面板设计是否符合人体工学就显得比较重要了。</span>
+      <span>6.跑步机马达：跑步机马达方面，除了功率要适合体重外，马达运行时候的平稳性以及有无噪音这两方面也需要注意下。</span>
+      <span>7.跑步机的速度和坡度调节功能：这两点要根据自己和家人跑步机锻炼的情况来定，在选购的时候就应该了解跑步机的预设速度和坡度调节功能是否满足自身的需要。</span>
+      <span>8.跑步机功能切换时的稳定性如何：有些时候，我们需要在跑步锻炼的时候随时切换跑步锻炼的模式，如果在跑步模式切换的时候，跑步机反应剧烈致使身体摇晃，这样的跑步机的稳定性就很差。</span>
+      <span>9.跑步带是否平稳：跑步带是我们在跑步机上锻炼时候比较重要的主要部件之一，如果跑步带在跑步的时候运行不平稳，经常出现打滑、间断以及跑偏等现象，则对跑步锻炼不利。如果是长时间使用的跑步带跑偏的调整可以参考下跑步机跑偏时如何调整一文。</span>
+      <span>10.跑步机的整体体验：一般设计精良的跑步机，其跑步者的适应性都很好，当您在选购跑步机前，把以上所有的功能都体验完毕后，您的整体感受如何？</span>
+    </div>
+    <div class="floote">联系电话:13121921833</div>
+  </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+<style scoped>
+  body{
+    background-color: red;
+  }
+  .warp{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: #989292;
+    color: #fff;
+  }
+  .header{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin: 0.8rem 0.6rem;
+    flex-wrap:wrap;
+    font-weight: 500;
+    font-size: 2.6rem;
+    font-family: "DFKai-SB";
+  }
+  .content{
+    display: flex;
+    flex-direction: column;
+
+    
+    padding: 0 1.5rem;
+    margin: 0 0 3.5rem 0;
+    font-family: "DFKai-SB";
+  }
+  .content span{
+    display: inline-block;
+    text-indent: 2.7rem;
+    padding: 1.5rem 0;
+    letter-spacing: 0.3rem;
+  }
+  .floote{
+    padding: 0 1.5rem 2rem 1.5rem;
+    text-align: center;
+  }
+</style>
+
 

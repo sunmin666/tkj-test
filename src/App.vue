6fafbc100c1b6a2f5c9359489f25d0ce96aaf925
Sun Min <1404280914@qq.com>
2020-06-29 15:36:06 +0800

    孙敏首页

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+     <router-view></router-view>
   </div>
 </template>
 
 <style>
-#app {
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 }
@@ -28,5 +28,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
